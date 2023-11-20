a0dabaa9bf70171a57bbffc707c6383d46e92839
Ridha Arlian Alhaqie <66559097+ridha-arlian@users.noreply.github.com>
2023-11-21 01:43:57 +0700

    Update New Page

---
+++ Javascript/hotels.js
@@ -0,0 +1,13 @@
+$(function()
+{
+    $('input[name="daterange"]').daterangepicker(
+        {
+            ssingleDatePicker: 'true',
+            showShortcuts: 'false',
+            showTopbar: 'false'
+        }
+    );
+
+    $('input[name="daterange"]').val('');
+    $('input[name="daterange"]').attr("placeholder","Check-in/Check-out");
+});
